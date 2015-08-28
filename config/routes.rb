3aa4d55caadd4bffdd2f92de74dc0ec7c28e4612
yosuke0315 <wonderfulworld0315@gmail.com>
2015-08-28 19:14:49 +0900

    routes setting

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :upload_images
+  resources :upload_images, only: [:index, :new, :upload, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
