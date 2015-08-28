adf2dee111898abf1513b554ed8fd02b68f88533
Sou Konishi <srm81821@gmail.com>
2015-08-29 03:15:20 +0900

    Add responsive

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150828084421) do
+ActiveRecord::Schema.define(version: 20150828141942) do
 
   create_table "upload_images", force: :cascade do |t|
-    t.string   "image"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
   create_table "users", force: :cascade do |t|
